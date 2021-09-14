86bd37b17531952d5723a56a97b8dfc246ed84e7
Jeff <jefferson.santos@maxmilhas.com.br>
2021-09-14 09:34:21 -0300

    style detail

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { useRouteMatch } from "react-router";
+import { Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import LogoImg from "../../assets/logo.svg";
+
+import { Header, RepositoryInfo, Issues } from "./styles";
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +13,54 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={LogoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://findicons.com/files/icons/178/popo_emotions/128/haha.png"
+            alt="Teste"
+          />
+          <div>
+            <strong>Lorem.</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={`teste`}>
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
